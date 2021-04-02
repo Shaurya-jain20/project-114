f716d8c269cab16fb271cde1361e7fcf151d28b8
Shaurya-jain20 <76279234+Shaurya-jain20@users.noreply.github.com>
2021-04-02 13:06:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload()
+{
+
+}
+function setup()
+{
+    canvas = createCanvas(210,210);
+    canvas.center();
+}
+
+function draw()
+{
+
+}
+function take_snapshot()
+{
+    save("mustache.jpg");
+}
